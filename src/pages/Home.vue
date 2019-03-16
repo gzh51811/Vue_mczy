393811b35632cb06e93615000b79b0d0cf931789
Wusi Yu <784819492@qq.com>
2019-03-16 13:32:26 +0800

    首页main部分

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -22,9 +22,9 @@
       <section class="banner">
         <section class="swiper-container">
           <section class="swiper-wrapper">
-            <section class="swiper-slide">Slide 1</section>
-            <section class="swiper-slide">Slide 2</section>
-            <section class="swiper-slide">Slide 3</section>
+            <section class="swiper-slide" v-for="banners in banner" :key="banners.imgurl">
+              <img :src="banners.imgurl" class="autoimg">
+            </section>
           </section>
           <!-- 如果需要分页器 -->
           <section class="swiper-pagination"></section>
@@ -91,11 +91,90 @@
         </section>
       </section>
     </header>
-
     <!-- main1 -->
-    <section class="main1">
-      <section></section>
+    <!-- <section class="main1"> -->
+    <section class="promo2m">
+      <a href="javascript:void(0)">
+        <section
+          class="promo2mA"
+          style="background: url(../../../assets/images/promo1.jpg)no-repeat right 1.75em;
+    background-size: 2.7rem auto;border-right:.0625rem solid #eee"
+        >
+          <section class="promo2m_titleA">葆丽美指定产品</section>
+          <section class="promo2m_taglineA">送值千元希思黎面膜</section>
+          <section class="promo2_go_btn">立即抢购 GO</section>
+        </section>
+      </a>
+      <a href="javascript:void(0)">
+        <section
+          class="promo2mA"
+          style="background: url(../../../assets/images/promo2.jpg)no-repeat right 1.75em;
+    background-size: 2.7rem auto; "
+        >
+          <section class="promo2m_titleA">莎莎Outlet</section>
+          <section class="promo2m_taglineA">尾单专区 底价抢好货</section>
+          <section class="promo2_go_btn">立即抢购 GO</section>
+        </section>
+      </a>
+      <a href="javascript:void(0)">
+        <section
+          class="promo2mC"
+          style="
+          background:url(../../../assets/images/promo3.jpg)no-repeat center 54px;background-size:65% auto;border-left:none"
+        >
+          <section class="promo2m_titleB">新人福利专场</section>
+          <section class="promo2m_taglineB">领888元见面礼</section>
+        </section>
+      </a>
+      <a href="javascript:void(0)">
+        <section
+          class="promo2mC"
+          style="background:url(../../../assets/images/promo4.jpg)no-repeat center 54px;background-size:65% auto;"
+        >
+          <section class="promo2m_titleB">会员俱乐部</section>
+          <section class="promo2m_taglineB">积分抵现计划</section>
+        </section>
+      </a>
+      <a href="javascript:void(0)">
+        <section
+          class="promo2mC"
+          style="background:url(../../../assets/images/promo5.jpg)no-repeat center 54px;background-size:65% auto;"
+        >
+          <section class="promo2m_titleB">VIP福利场</section>
+          <section class="promo2m_taglineB">3月VIP尊享</section>
+        </section>
+      </a>
     </section>
+    <!-- </section> -->
+    <!-- 每日必看 -->
+    <div class="sasa_mustcheck_head">
+      <img src="../assets/images/must_check.png">
+    </div>
+
+    <div class="mustcheck">
+      <div style="opacity: 1; display: block;">
+        <div
+          class="mustcheck_item"
+          style="transform-origin: 0px 0px 0px; opacity: 1; transform: scale(1, 1);"
+          v-for="(mustchecks) in  mustcheck"
+          :key="mustchecks.title"
+        >
+          <a href="javascript:void(0)" target="_blank">
+            <img class="mustcheck_lg_img" :src="mustchecks.imgurl">
+          </a>
+          <div class="mustcheck_title">
+            <div class="mustcheck_titName">{{mustchecks.titname}}</div>
+            <div class="mustcheck_subtitName">{{mustchecks.subtitname}}</div>
+            <div class="mustcheck_date">{{mustchecks.title}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 限时特卖 -->
+    <div class="limit_lg_head">
+      <img src="../assets/images/limitted_offer.png">
+    </div>
   </section>
 </template>
    
@@ -107,6 +186,26 @@ import "swiper/dist/css/swiper.min.css";
 export default {
   data() {
     return {
+      //轮播图
+      banner: [
+        {
+          imgurl: "../assets/images/banner1.jpg"
+        },
+        {
+          imgurl: "../assets/images/banner2.jpg"
+        },
+        {
+          imgurl: "../assets/images/banner3.jpg"
+        },
+        {
+          imgurl: "../assets/images/banner4.jpg"
+        },
+        {
+          imgurl: "../assets/images/banner5.jpg"
+        }
+      ],
+
+      // 广告轮播
       advertisement: [
         {
           time: "保税仓物流公告 2019.03.12",
@@ -128,6 +227,39 @@ export default {
           time: "保税仓物流公告 2019.06.23",
           title: "国内保税仓海关盘点通知"
         }
+      ],
+      // 每日必看
+      mustcheck: [
+        {
+          imgurl: "../assets/images/mustcheck1.jpg",
+          titname: "美白淡斑神器",
+          subtitname: "OLAY 淡斑小白瓶275元",
+          title: "让你美到发光"
+        },
+        {
+          imgurl: "../assets/images/mustcheck2.jpg",
+          titname: "敏感肌救星",
+          subtitname: "悦木之源菌菇水259 get!",
+          title: "舒缓肌肤敏感"
+        },
+        {
+          imgurl: "../assets/images/mustcheck3.jpg",
+          titname: "OMG显白口红",
+          subtitname: "娇兰亲亲唇膏231元！",
+          title: "网红必BUY推荐"
+        },
+        {
+          imgurl: "../assets/images/mustcheck4.jpg",
+          titname: "人气时尚彩妆品牌",
+          subtitname: "CYBER COLORS",
+          title: "控油清爽粉饼98元必抢！"
+        },
+        {
+          imgurl: "../assets/images/mustcheck5.jpg",
+          titname: "美白淡斑神器",
+          subtitname: "OLAY 淡斑小白瓶666元",
+          title: "让你美到发光2"
+        }
       ]
     };
   },
@@ -143,7 +275,7 @@ export default {
     //广告轮播
     let mySwiper3 = new Swiper(".swiper-container2", {
       direction: "vertical",
-      slidesPerView: 3,
+      // slidesPerView: 3,
       loop: true,
       centeredSlides: true,
       autoplay: true
